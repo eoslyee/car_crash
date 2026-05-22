80bd9a53b344392eb9e7851c38b65e5ed4ae6434
Estella Yee <eyee60@stuy.edu>
2026-05-22 11:24:36 -0400

    Car making

---
+++ Glowscript.py
@@ -0,0 +1,16 @@
+# Web VPython 3.2
+
+#Defining the object 
+car = box(pos=vec(-3,-3,0),axis=vec(1,0,0), size=vec(1,1,3), color=color.red, texture=textures.wood,
+make_trail = True)
+
+#Defining initial velocity/acceleration/position 
+velocity = vec(2, 1, 0)
+dt = 0.01 
+
+#Simple animation 
+while True: 
+    rate(60) 
+    print(car.pos)
+    car.pos = car.pos + velocity * dt 
+    
