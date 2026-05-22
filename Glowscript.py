73f051bc2c65cdcee13720d5a71c3015e03ee84e
Estella Yee <eyee60@stuy.edu>
2026-05-22 11:38:38 -0400

    added target

--- Glowscript.py
+++ Glowscript.py
@@ -1,11 +1,12 @@
 # Web VPython 3.2
 
 #Defining the object 
-car = box(pos=vec(-3,-3,0),axis=vec(1,0,0), size=vec(1,1,3), color=color.red, texture=textures.wood,
-make_trail = True)
+car = box(pos=vec(-3,-3,0),axis=vec(1,0,0), size=vec(1,1,3), color=color.red, texture=textures.metal)
+
+target = box(pos=vec(3, 3, -5), size = vec(3, 3, 3), color = color.green, texture=textures.wood)
 
 #Defining initial velocity/acceleration/position 
-velocity = vec(2, 1, 0)
+velocity = vec(2, 1, -0.05)
 dt = 0.01 
 
 #Simple animation 
@@ -13,4 +14,9 @@ while True:
     rate(60) 
     print(car.pos)
     car.pos = car.pos + velocity * dt 
+    
+    
+while car.pos == target.pos: 
+    rate(0)
+    print("target was hit!")
     
