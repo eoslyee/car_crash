459063db9ad5324ae3ac789229653643dd694251
Estella Yee <eyee60@stuy.edu>
2026-05-27 22:34:48 -0400

    moving car and button properties. Retiring for the night :)

--- Glowscript.py
+++ Glowscript.py
@@ -1,4 +1,4 @@
-# Web VPython 3.2
+Web VPython 3.2
 
 #Animation Window 
 scene.width = 900
@@ -16,27 +16,27 @@ for xpos in range(-28, 30, 4):
     color=color.white, opacity=0.5))
 
 surface_settings = {
-    "asphalt": color.gray(0.35),
-    "wet road": vec(0.3, 0.35, 0.45), 
-    "ice": vec(0.75, 0.88, 1.0),
+    "Asphalt": color.gray(0.35),
+    "Wet road": vec(0.3, 0.35, 0.45), 
+    "Ice": vec(0.75, 0.88, 1.0),
 }
 
 car_selection = {
-    "small  (800 kg)":  {"mass": 800,  "scale": 0.85, "color": color.red},
-    "medium (1400 kg)": {"mass": 1400, "scale": 1.0,  "color": color.blue},
-    "large  (2500 kg)": {"mass": 2500, "scale": 1.2,  "color": color.green},
+    "Small":  {"mass": 800,  "scale": 0.85, "color": color.red},
+    "Medium": {"mass": 1400, "scale": 1.0,  "color": color.blue},
+    "Large": {"mass": 2500, "scale": 1.2,  "color": color.green},
 }
 
 target_selection = {
     "Stationary Tree":    {"mass": 5000, "moving": False, "color": color.green},
-    "Parked Car (1400 kg)": {"mass": 1400, "moving": False, "color": color.cyan},
-    "Deer (120 kg)":      {"mass": 120,  "moving": True,  "color": color.orange},
-    "Truck (6000 kg)":    {"mass": 6000, "moving": False, "color": color.red},
+    "Parked Car": {"mass": 1400, "moving": False, "color": color.cyan},
+    "Deer":      {"mass": 120,  "moving": True,  "color": color.orange},
+    "Truck":    {"mass": 6000, "moving": False, "color": color.red},
 }
 
 state = {
     "surface":     "Asphalt",
-    "car_type":    "Medium (1400 kg)",
+    "car_type":    "Medium",
     "target_type": "Stationary Tree",
     "car_speed":   15.0,      # m/s (will use later) 
     "elasticity": 0.5,       # 0 = perfectly inelastic, 1 = elastic
@@ -58,10 +58,10 @@ def clear(obj_list):
     
 def build_car(car_type, start_x = -12): 
     """Builds a simple car shape """
-    clear_objects(current_car)
+    clear(current_car)
     info = car_selection[car_type]
     scale = info["scale"]
-    col - info["color"] 
+    col = info["color"] 
     
     body = box(pos=vec(start_x, 0.45*scale, 0),
               size = vec(3.5*scale, 0.8*scale, 1.8*scale), 
@@ -76,7 +76,7 @@ def build_car(car_type, start_x = -12):
         vec(start_x + 1.1*scale,  r,  0.95*scale),
         vec(start_x + 1.1*scale,  r, -0.95*scale),
         vec(start_x - 1.1*scale,  r,  0.95*scale),
-        vec(start_x - 1.1*scae,  r, -0.95*scale),
+        vec(start_x - 1.1*scale,  r, -0.95*scale),
     ]
     
     for wp in wheel_pos: 
@@ -86,20 +86,28 @@ def build_car(car_type, start_x = -12):
     current_car.append(body)
     current_car.append(cabin)
 
+# Building the Target 
+#
+#def build_target(target_type, target_x=8): 
+#    """ Build Target""" 
+#    clear(current_target)
+#    info = target_selection[target_type]
+#    col = info["color"]
+
 # User Controls 
 scene.append_to_caption("\n")
  
 # Setting Surface 
 
 scene.append_to_caption("<b>Surface:</b>  ")
+
 def set_ground(b): 
     state["surface"] = b.text
-    ground.color = surface_colors[b.text]
-    ground.color = surface_colors["Asphalt"] 
+    ground.color = surface_settings[b.text]
 
-button(text="Asphalt",  bind=set_ground)
+button(text="Asphalt", bind=set_ground)
 button(text="Wet Road", bind=set_ground)
-button(text="Ice",      bind=set_ground)
+button(text="Ice", bind=set_ground)
 scene.append_to_caption("\n\n")
 
 # Setting Car Size
@@ -107,14 +115,64 @@ scene.append_to_caption("\n\n")
 scene.append_to_caption("<b>Car Type:</b> ") 
 def set_size(b): 
     state["car_type"] = b.text
-    build_car(state["car_type"])
+    build_car(b.text)
 
-button(text="Small (800 kg)",  bind=set_ground)
-button(text="Medium (1400 kg)", bind=set_ground)
-button(text="Large (2500 kg)",      bind=set_ground)
+button(text="Small", bind=set_size)
+button(text="Medium", bind=set_size)
+button(text="Large", bind=set_size)
 scene.append_to_caption("\n\n")
-    
+
+# Setting Target 
+scene.append_to_caption("<b>Target:</b>  ")
  
+def set_target(b):
+    state["target_type"] = b.text
+    build_target(b.text)
+
+button(text="Tree", bind=set_target)
+button(text="Parked Car", bind=set_target)
+button(text="Deer", bind=set_target)
+button(text="Truck", bind=set_target)
+scene.append_to_caption("\n\n")
+
+# Start & Reset Buttons 
+status_text = wtext(text="<b>Status:</b> Press Start to run.")
+scene.append_to_caption("  ")
+ 
+def start_sim(b):
+    state["reset_flag"] = True
+    state["running"]    = True
+    
+button(text="Start / Restart", bind=start_sim)
+
+    
+# ACTUAL SCENE BUILGIND
+build_car(state["car_type"]) 
+# build_target(state["target_type"])
+
+# Animation Loop 
+dt = 0.005
+t = 0.0
+
+while True: 
+    rate(200) # 200 steps/sec 
+    
+    #reset
+    
+    # Car movement 
+    dx = state["car_speed"] * dt #will change to nonconstant velocity later
+    
+    for i in current_car:
+        i.pos.x += dx
+        
+    car_x = current_car[-1].pos.x
+#    target_x = current_target[0].pos.x
+#    
+#    if car_x >= target_x - 2.0: 
+#        state["running"] = False 
+#        status_text.text = "<b>Status:</b> Collision!"
+    
+    t += dt
 
 # Graph 
 
